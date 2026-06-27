64183a49a2c7ffe17777bd565c39d3b946469d20
Marlon Acosta <166179081+MattMarlo@users.noreply.github.com>
2026-06-26 22:57:20 -0500

    cambios para solucionar la subida de archivos en clientes

--- app/Http/Controllers/ClienteController.php
+++ app/Http/Controllers/ClienteController.php
@@ -77,6 +77,9 @@ class ClienteController extends Controller
                 //  Guarda en storage/app/public/clientes/
                 $ruta = $request->file('archivo')->store('clientes', 'public');
                 $datos['archivo'] = $ruta; //  Asignar ruta relativa
+                Log::info('Archivo subido correctamente', ['ruta' => $ruta]); // LOG 2
+            }else{
+                Log::warning('No se recibió ningún archivo en la petición'); // LOG 3
             }
             //$this->clienteService->guardarCliente($datos);
             $cliente=$this->clienteService->guardarCliente($datos);
